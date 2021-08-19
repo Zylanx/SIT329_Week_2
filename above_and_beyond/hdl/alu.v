42a42bc3332b1f51ab8bfa170486b0c8d8aca180
Zylanx <zylanxdragon@gmail.com>
2021-08-19 16:49:16 +1000

    add .gitignore

--- above_and_beyond/hdl/alu.v
+++ above_and_beyond/hdl/alu.v
@@ -13,14 +13,14 @@ module alu
 	always @* begin
 		// Set overflow to default to 0
 		// This removes any extra latches and keeps it combinational
-		overflow = 0;
+		overflow <= 0;
 		
 		case(op)
-			2'b00: {overflow, out} = a + b; // Add
-			2'b01: {overflow, out} = a - b; // Subtract
-			2'b10: out = ~a; // Not
-			2'b11: out = a & b; // And
-			default: out = a + b;
+			2'b00: {overflow, out} <= a + b; // Add
+			2'b01: {overflow, out} <= a - b; // Subtract
+			2'b10: out <= ~a; // Not
+			2'b11: out <= a & b; // And
+			default: out <= a + b;
 		endcase
 	end
 
